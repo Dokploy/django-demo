e7bf234486779246bfbbe5a155e88c41d094023b
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-05-01 02:05:31 -0600

    refactor: update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
